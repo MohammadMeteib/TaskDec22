ee05b49db6b7fc9bb7a4eaa0efd23e7b6ce76b40
ABK <ABK@DESKTOP-PTCS16K>
2024-12-22 14:55:50 +0300

    Add project files.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Collections;
+using System.Linq.Expressions;
+ArrayList Fruits = new ArrayList()
+{
+    "apple", "cherry", "banana"
+};
+foreach (var f in Fruits)
+{
+    Console.WriteLine(f);
+}
+Fruits.Insert(2, "Orange");
+Fruits.Remove(0);
+Console.WriteLine("New list");
+foreach (var f in Fruits)
+{
+    Console.WriteLine(f);
+}
+Console.WriteLine("type any fruit:");
+string check = Console.ReadLine().ToLower();
+if (Fruits.Contains(check))
+{
+    Console.WriteLine("it exists at " + Fruits.IndexOf(check));
+}
+else Console.WriteLine("it doesn't");
+    Console.WriteLine("The number of elements is: ");
+    Console.WriteLine(Fruits.Count);
+
